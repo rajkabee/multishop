0eb137393900e50f67d9e1899f9fc9c33bd9d7c9
unknown <rajkabee@gmail.com>
2022-09-02 10:11:00 +0545

    main

---
+++ src/main/java/com/example/demo/AdminPanelComtroller.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminPanelComtroller {
+	@RequestMapping("/")
+	public String home() {
+		return "admin";
+	}
+	
+	@RequestMapping("/products")
+	public String products() {
+		return "products";
+	}
+	
+	@RequestMapping("/products/addProduct")
+	public String addProduct() {
+		return "addProductForm";
+	}
+}
